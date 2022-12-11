d215d7d95a9f6bc4fa43f219262090127954cc2b
fantozes <fantozes@gmail.com>
2022-12-11 09:05:20 +0900

    add library

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-if __name__ == '__main__':
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import time
+import datetime
+import os
+
+
+def main():
     pass
+
+
+if __name__ == '__main__':
+    main()
