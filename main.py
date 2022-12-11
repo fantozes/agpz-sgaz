47c14c76547fa8120b1be311984778992dd04c22
fantozes <fantozes@gmail.com>
2022-12-11 09:39:52 +0900

    abrakadabra

--- main.py
+++ main.py
@@ -8,9 +8,32 @@ import time
 import datetime
 import os
 
+urlLogin = 'https://agpz.sgaz.pro/faces/zeroLevelOOP'
+
+urlWork = 'https://agpz.sgaz.pro/faces/page/contracts/48899/build-tracker/tasks'
+
+options = Options()
+
+options.add_argument("--disable-extensions")
+
+
+driver = webdriver.Chrome(executable_path='./chromedriver', options=options)
+
+
+def authorization():
+    print(f'Авторизация LOGIN: "ваш_логин", PASS: {"*" * len("ваш_пароль")}')
+
 
 def main():
-    pass
+
+    print(f'Переход на сайт: {urlLogin}')
+
+    driver.get(urlLogin)
+    print(f'Получен заголовок сайта: {driver.title}')
+
+    time.sleep(2)
+
+    authorization()
 
 
 if __name__ == '__main__':
